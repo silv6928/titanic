cdc6de3eadb6dd3fe663f2d0609f0d80766cf920
Tony Silva <tony.silva@ou.edu>
2017-05-19 10:09:34 -0500

    predictive modeling

--- titanic.py
+++ titanic.py
@@ -9,11 +9,18 @@ import random
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 '''
 Data Cleaning Operations
 -Loading Data
 -Fix missing values
 -Create feature vectors
+Visual Analysis
+Predictive Model Creation
 '''
 # Load Data
 titanic = pd.read_csv("C:/Users/Anthony Silva/silvat/titanic/train.csv")
@@ -29,10 +36,10 @@ del df_age
 
 # Create Feature Vectors for Pclass variable
 # Commented out because class had order to it
-df_pclass = pd.get_dummies(titanic["Pclass"])
-df_pclass.columns = ["pclass1","pclass2","pclass3"]
-titanic = pd.concat([titanic, df_pclass], axis=1)
-del df_pclass
+#df_pclass = pd.get_dummies(titanic["Pclass"])
+#df_pclass.columns = ["pclass1","pclass2","pclass3"]
+#titanic = pd.concat([titanic, df_pclass], axis=1)
+#del df_pclass
 
 # Create Feature Vectors for Embarked variable
 df_embark = pd.get_dummies(titanic["Embarked"])
@@ -65,12 +72,13 @@ del cl
 del i
 ## Price is Fare / Ticket Count, some tickets were purchased in pairs for a family
 ## Gets the price per person, so as not to inflate the amount paid by person.
-titanic.price = titanic.Fare / titanic.ticket_count
+titanic["price"] = titanic.Fare / titanic.ticket_count
 
 ##print(len(titanic["title"].unique())) #17 unique titles
-#df_title = pd.get_dummies(titanic["title"])
-#titanic = pd.concat([titanic, df_title], axis=1)
-#del df_title
+df_title = pd.get_dummies(titanic["title"])
+# Combine the various versions of Miss
+titanic = pd.concat([titanic, df_title], axis=1)
+del df_title
 
 df_CL = pd.get_dummies(titanic["CL"])
 titanic = pd.concat([titanic, df_CL], axis=1)
@@ -86,21 +94,132 @@ titanic.loc[titanic.Age >= 60, "age_grp"] = 2
 titanic["age_grp"] = titanic["age_grp"].astype(int)
 titanic["Survived"] = titanic["Survived"].astype('category')
 
+
 # Data Exploration
 # Which variables help predict survival better
 # More people died then survived.
+'''
 plt.figure()
 sns.countplot(x="Survived", data=titanic)
 plt.figure()
 sns.countplot(x="age_grp", hue="Survived", data=titanic)
+# If embarking from S, you were more likely to not survive.
 plt.figure()
 sns.countplot(x="Embarked", hue="Survived", data=titanic)
+# Traveling by yourself made you more likely to not survive
 plt.figure()
 sns.countplot(x="famsize", hue="Survived", data=titanic)
+# Being in third class made it more likely to not survive
+plt.figure()
+sns.countplot(x="Pclass", hue="Survived", data=titanic)
+'''
 
 ### Generate a Predictive Model ###
 # Drop out unused columns for Machine Learning.
-#titanic = titanic.drop(["Embarked", "Sex", "title",
-#                        "Ticket", "CL", "Cabin", "Name"], axis=1)
+titanic = titanic.drop(["Embarked", "Sex", "title",
+                        "Ticket", "CL", "Cabin", "Name"], axis=1)
+
+predictors = list(titanic.columns.values)[1:]
+target = list(titanic.columns.values)[0]
+
+# Create training and test sets at 70% train, 30% test
+# 624 values in training and 267 in test based on split.
+train, test = train_test_split(titanic, test_size=0.3)
+
+clf = LogisticRegression()
+clf.fit(train[predictors], train[target])
+predictions = clf.predict(test[predictors])
+actual = test[target]
+print("Accuracy for LogReg: ", clf.score(test[predictors], test[target]))
+print(classification_report(actual, predictions))
 
 
+# Considerations: Create Ticket Featuress for all of the different tickets
+del test
+
+'''
+Create Test Predictions for Kaggle
+'''
+test = pd.read_csv("C:/Users/Anthony Silva/silvat/titanic/test.csv")
+test = test.set_index("PassengerId")
+# Fix Missing Values
+test["Age"] = test["Age"].fillna(test["Age"].mean())
+test["Age"] = test.Age.round()
+test["Fare"] = test["Fare"].fillna(test["Fare"].mean())
+test["Fare"] = test.Fare.round(2)
+test["Embarked"] = test["Embarked"].fillna("S")
+test["Cabin"] = test["Cabin"].fillna("Z")
+# Create Feature Vectors for Sex variable
+df_age = pd.get_dummies(test["Sex"])
+test = pd.concat([test, df_age], axis=1)
+del df_age
+
+
+
+# Create Feature Vectors for Embarked variable
+df_embark = pd.get_dummies(test["Embarked"])
+test = pd.concat([test, df_embark], axis=1)
+
+del df_embark
+#
+## Create new features for Family Size
+test["famsize"] = test["SibSp"] + test["Parch"] + 1
+## Create a new feature for Title, Cabin Letter, Price of a ticket
+## Title is Mr, Mrs, Miss ,etc
+## Cabin Letter is the beginning letter of the cabin, if null then N
+## Ticket Count is the number of tickets in the data set. 
+tickets = test["Ticket"].value_counts()
+for i in test.itertuples():
+    title = i[2].replace(' ','').replace('.',',').split(',')
+    cl = str(i[9])
+    cl = cl.upper()
+    if cl[0] == "C": 
+        cl = "CC"
+    else:
+        cl = cl[0]
+    ticket = tickets[i[7]]
+    test.loc[i[0], "title"] = title[1]
+    test.loc[i[0], "CL"] = cl
+    test.loc[i[0], "ticket_count"] = ticket
+del title
+del ticket
+del tickets
+del cl
+del i
+## Price is Fare / Ticket Count, some tickets were purchased in pairs for a family
+## Gets the price per person, so as not to inflate the amount paid by person.
+test["price"] = test.Fare / test.ticket_count
+
+##print(len(test["title"].unique())) #17 unique titles
+df_title = pd.get_dummies(test["title"])
+test = pd.concat([test, df_title], axis=1)
+del df_title
+
+df_CL = pd.get_dummies(test["CL"])
+test = pd.concat([test, df_CL], axis=1)
+del df_CL
+
+
+# Create an Age Group Feature
+# 0 if a child, 1 if an adult, 2 if a senior.
+test.loc[test.Age < 18, "age_grp"] = 0
+test.loc[(test.Age >= 18) & (test["Age"] < 60), "age_grp"] = 1
+test.loc[test.Age >= 60, "age_grp"] = 2
+test["age_grp"] = test["age_grp"].astype(int)
+
+
+### Generate a Predictive Model ###
+# Drop out unused columns for Machine Learning.
+test = test.drop(["Embarked", "Ticket", "Sex", "title",
+                         "CL", "Cabin", "Name"], axis=1)
+test
+# Return the Columns in the Training set not in the test set
+cols = list(set(titanic.columns.values) - set(test.columns.values))
+for i in cols:
+    test[i] = 0
+results = pd.Series(clf.predict(test[predictors]), name="Survived")
+PassengerId = pd.Series(test.index.values, name="PassengerId")
+results = pd.concat([PassengerId, results], axis=1)
+results = results.set_index("PassengerId")
+
+#results.to_csv("submission_1.csv")
---
+++ titanic2.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+Tony Silva
+
+Condensed Titanic Practice
+"""
+import pandas as pd
+import numpy
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression, LinearRegression, SGDClassifier
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+train = pd.read_csv("C:/Users/Anthony Silva/silvat/titanic/train.csv")
+train = train.set_index("PassengerId")
+test = pd.read_csv("C:/Users/Anthony Silva/silvat/titanic/test.csv")
+test = test.set_index("PassengerId")
+titanic = train.loc[:, "Pclass":]
+titanic = titanic.append(test)
+titanic = pd.concat([titanic, train["Survived"]], axis=1)
+
+titanic["Cabin"] = titanic["Cabin"].fillna("Z")
+titanic["Embarked"] = titanic["Embarked"].fillna("S")
+titanic["Fare"] = titanic["Fare"].fillna(titanic["Fare"].mean())
+titanic["Fare"] = titanic["Fare"].round(2)
+df_age = pd.get_dummies(titanic["Sex"])
+titanic = pd.concat([titanic, df_age], axis=1)
+del df_age
+
+
+# Create Feature Vectors for Pclass variable
+# Commented out because class had order to it
+#df_pclass = pd.get_dummies(titanic["Pclass"])
+#df_pclass.columns = ["pclass1","pclass2","pclass3"]
+#titanic = pd.concat([titanic, df_pclass], axis=1)
+#del df_pclass
+
+# Create Feature Vectors for Embarked variable
+df_embark = pd.get_dummies(titanic["Embarked"])
+titanic = pd.concat([titanic, df_embark], axis=1)
+del df_embark
+#
+## Create new features for Family Size
+titanic["famsize"] = titanic["SibSp"] + titanic["Parch"] + 1
+## Create a new feature for Title, Cabin Letter, Price of a ticket
+## Title is Mr, Mrs, Miss ,etc
+## Cabin Letter is the beginning letter of the cabin, if null then N
+## Ticket Count is the number of tickets in the data set. 
+tickets = titanic["Ticket"].value_counts()
+for i in titanic.itertuples():
+    title = i[2].replace(' ','').replace('.',',').split(',')
+    cl = str(i[10])
+    cl = cl.upper()
+    if cl[0] == "C": 
+        cl = "CC"
+    else:
+        cl = cl[0]
+    ticket = tickets[i[7]]
+    titanic.loc[i[0], "title"] = title[1]
+    titanic.loc[i[0], "CL"] = cl
+    titanic.loc[i[0], "ticket_count"] = ticket
+del title, ticket, tickets, cl, i
+
+titanic["price"] = titanic.Fare / titanic.ticket_count
+titanic["price"] = titanic["price"].round(2)
+titanic["Age"] = titanic["Age"].round()
+
+df_title = pd.get_dummies(titanic["title"])
+titanic = pd.concat([titanic, df_title], axis=1)
+del df_title
+
+df_CL = pd.get_dummies(titanic["CL"])
+titanic = pd.concat([titanic, df_CL], axis=1)
+del df_CL
+
+
+titanic = titanic.drop(["Embarked", "Fare", "Sex", "title", "Ticket", "CL", "Cabin", "Name"], axis=1)
+
+
+# Create a Linear Regression Model to predict the Age of the null Ages
+age_pred = LinearRegression()
+train = titanic.loc[titanic.Age.notnull(), :]
+train = train.drop("Survived", axis=1)
+train_X = train.drop("Age", axis=1)
+train_Y = train["Age"]
+age_pred.fit(train_X, train_Y)
+#for i in range(len(list(train_X.columns.values))):
+#    print(list(train_X.columns.values)[i], list(age_pred.coef_)[i])
+#print(age_pred.score(train_X, train_Y))
+test = titanic.loc[titanic.Age.isnull(),:]
+test = test.drop("Survived", axis=1)
+test_X = test.drop("Age", axis=1)
+titanic.loc[titanic.Age.isnull(),"Age"] = age_pred.predict(test_X)
+del test,test_X,age_pred
+
+# Create an age group feature.
+titanic.loc[titanic.Age < 18, "age_grp"] = 0
+titanic.loc[(titanic.Age >= 18) & (titanic["Age"] < 60), "age_grp"] = 1
+titanic.loc[titanic.Age >= 60, "age_grp"] = 2
+#titanic["age_grp"] = titanic["age_grp"].astype(int)
+titanic["Survived"] = titanic["Survived"].astype('category')
+
+train = titanic.loc[titanic.Survived.notnull(), :]
+train, vald = train_test_split(train, test_size=.3)
+test = titanic.loc[titanic.Survived.isnull(), :]
+test = test.drop("Survived", axis=1)
+train_X = train.drop("Survived", axis=1)
+train_Y = train["Survived"]
+vald_X = vald.drop("Survived", axis=1)
+actual = vald["Survived"]
+
+#Logistic Regression Model, accuracy of .77512
+clf = LogisticRegression()
+clf.fit(train_X, train_Y)
+predictions = clf.predict(vald_X)
+print("Logisitic Regression Model")
+print(clf.score(vald_X, actual))
+print(confusion_matrix(actual, predictions, labels=[0,1]))
+
+# Random Forest Classifier, accuracy of .78947
+clf = RandomForestClassifier(n_estimators=500,criterion="entropy", min_samples_leaf=4)
+clf.fit(train_X, train_Y)
+predictions = clf.predict(vald_X)
+print("Random Forest Classifier")
+print(clf.score(vald_X, actual))
+print(confusion_matrix(actual, predictions, labels=[0,1]))
+
+results = pd.Series(clf.predict(test), name="Survived")
+results = results.astype(int)
+PassengerId = pd.Series(test.index.values, name="PassengerId")
+results = pd.concat([PassengerId, results], axis=1)
+results = results.set_index("PassengerId")
+results.to_csv("submission_2.csv")
